8d85809a4cdda47d285ccaaaa4225b91b72511d0
Pablo Álvarez Calvo <pabloalvarezcalvo@gmail.com>
2023-09-06 14:33:03 +0200

    Race class

--- src/main/java/model/Race.java
+++ src/main/java/model/Race.java
@@ -1,2 +1,46 @@
-package model;public class Race {
+package model;
+
+public class Race {
+    private long id;
+    private String name;
+    private RaceType type;
+
+    public Race(long id, String name, RaceType type) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public RaceType getType() {
+        return type;
+    }
+
+    public void setType(RaceType type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "Race{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", type=" + type +
+                '}';
+    }
 }
