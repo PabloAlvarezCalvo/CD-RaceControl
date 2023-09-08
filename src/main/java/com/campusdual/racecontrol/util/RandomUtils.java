622705dd7dfeb2d89bf797746a1257de3df702c2
Pablo Álvarez Calvo <pabloalvarezcalvo@gmail.com>
2023-09-08 13:52:15 +0200

    New features for the Utils class

--- src/main/java/com/campusdual/racecontrol/util/RandomUtils.java
+++ src/main/java/com/campusdual/racecontrol/util/RandomUtils.java
@@ -1,2 +1,12 @@
-package com.campusdual.racecontrol.util;public class RandomUtils {
+package com.campusdual.racecontrol.util;
+
+import java.util.Random;
+
+public class RandomUtils {
+    public static Random r = new Random();
+
+    public static int getRandomIntInRange(int min, int max){
+        max++;
+        return r.nextInt(max - min) + min;
+    }
 }
