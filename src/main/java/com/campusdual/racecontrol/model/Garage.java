4abafd12a712ac5be9e93d94674870d8cb864a29
Pablo Álvarez Calvo <pabloalvarezcalvo@gmail.com>
2023-09-11 13:24:32 +0200

    Garages: Modified to use JsonUtils and load cars.json to create garages.json

--- src/main/java/com/campusdual/racecontrol/model/Garage.java
+++ src/main/java/com/campusdual/racecontrol/model/Garage.java
@@ -1,13 +1,13 @@
 package com.campusdual.racecontrol.model;
 
+import com.campusdual.racecontrol.util.JsonUtils;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
-import java.io.FileWriter;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Garage {
+    private static final String GARAGE_TAG = "garages";
     private static final String GARAGE_ID = "id";
     private static final String GARAGE_NAME = "name";
     private static final String GARAGE_CARS = "cars";
@@ -87,52 +87,60 @@ public class Garage {
         return jsonArray;
     }
 
-    public static void exportJSONToFile(ArrayList<Garage> garages, String filename){
+    public static void exportGaragesToJSONFile(ArrayList<Garage> garages, String filename){
+        JSONObject jsonObject = new JSONObject();
         JSONArray jsonGarages = new JSONArray();
 
         for (Garage g : garages){
             jsonGarages.add(g.exportGarage());
         }
-        try (
-                FileWriter fw = new FileWriter(filename)
-        ) {
-            fw.write(jsonGarages.toJSONString());
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
+
+        jsonObject.put(GARAGE_TAG, jsonGarages);
+
+        JsonUtils.exportJsonObjectToFile(jsonObject, filename);
     }
 
     public static void main(String[] args) {
+        List<ScoreCar> cars = new ArrayList<>();
         Garage garagePaco = new Garage("Garaje Paco");
-
-        ScoreCar car1 = new ScoreCar("Seat", "Ibiza");
-        ScoreCar car2 = new ScoreCar("Citroen", "Xsara");
-        ScoreCar car3 = new ScoreCar("Opel", "Corsa");
-
-        car1.setGarageName(garagePaco.getName());
-        car2.setGarageName(garagePaco.getName());
-        car3.setGarageName(garagePaco.getName());
-
-        garagePaco.getCars().add(car1);
-        garagePaco.getCars().add(car2);
-        garagePaco.getCars().add(car3);
-
         Garage tallerManolo = new Garage("Taller Manolo");
-        ScoreCar car4 = new ScoreCar("Volkswagen", "Polo");
-        ScoreCar car5 = new ScoreCar("Volkswagen", "Golf");
-        tallerManolo.getCars().add(car4);
-        tallerManolo.getCars().add(car5);
-
         Garage escuderiaLoli = new Garage("Escuderia Loli");
-        ScoreCar car6 = new ScoreCar("Ford", "Mustang");
-        escuderiaLoli.getCars().add(car6);
+
+        JSONObject jsonObject = JsonUtils.importJSONFile("cars.json");
+
+        assert jsonObject != null;
+        JSONArray carJsonArray = (JSONArray) jsonObject.get("Cars");
+        System.out.println(carJsonArray);
+
+        for (Object o : carJsonArray) {
+            cars.add(ScoreCar.importCar((JSONObject) o));
+        }
+
+        for (ScoreCar sc : cars){
+            if (sc.getGarageName().equals(garagePaco.getName())){
+                garagePaco.getCars().add(sc);
+            } else if (sc.getGarageName().equals(tallerManolo.getName())){
+                tallerManolo.getCars().add(sc);
+            } else if (sc.getGarageName().equals(escuderiaLoli.getName())){
+                escuderiaLoli.getCars().add(sc);
+            }
+        }
+
+        System.out.println("Garage Paco:");
+        System.out.println(garagePaco.getCars());
+
+        System.out.println("Taller Manolo:");
+        System.out.println(tallerManolo.getCars());
+
+        System.out.println("Escuderia Loli:");
+        System.out.println(escuderiaLoli.getCars());
 
         ArrayList<Garage> garages = new ArrayList<>();
         garages.add(garagePaco);
         garages.add(tallerManolo);
         garages.add(escuderiaLoli);
 
-        exportJSONToFile(garages, "garages.json");
+        exportGaragesToJSONFile(garages, "garages.json");
 
     }
 }
