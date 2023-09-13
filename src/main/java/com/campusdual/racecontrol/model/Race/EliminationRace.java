a3a96c1fc3e3e1cc8fdaa08986af8501b1f74006
Pablo Álvarez Calvo <pabloalvarezcalvo@gmail.com>
2023-09-13 09:10:19 +0200

    EliminationRace

--- src/main/java/com/campusdual/racecontrol/model/Race/EliminationRace.java
+++ src/main/java/com/campusdual/racecontrol/model/Race/EliminationRace.java
@@ -1,12 +1,37 @@
 package com.campusdual.racecontrol.model.Race;
 
+import org.json.simple.JSONObject;
+
+import java.util.ArrayList;
+
 public class EliminationRace extends Race {
     /*
      * Elimination races have preliminar warm up minutes, after that, the last car will be removed every minute
      * until only one remains
      */
+    private int warmUpTime;
 
-    public EliminationRace(long id, String name) {
-        super(id, name);
+    public EliminationRace(String name) {
+        super(name);
+        id = generateId();
+    }
+
+    public int getWarmUpTime() {
+        return warmUpTime;
+    }
+
+    public void setWarmUpTime(int warmUpTime) {
+        this.warmUpTime = warmUpTime;
+    }
+
+    @Override
+    public void startRace() {
+        /*TODO*/
+    }
+
+
+    @Override
+    public JSONObject exportRace() {
+        return null;
     }
 }
