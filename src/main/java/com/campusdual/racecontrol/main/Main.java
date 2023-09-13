9a7ed6287554a85cb57bba788b1c9c1fbc2ce82f
Pablo Álvarez Calvo <pabloalvarezcalvo@gmail.com>
2023-09-13 09:01:00 +0200

    Main class

--- src/main/java/com/campusdual/racecontrol/main/Main.java
+++ src/main/java/com/campusdual/racecontrol/main/Main.java
@@ -1,7 +1,10 @@
-package main;
+package com.campusdual.racecontrol.main;
+
+import com.campusdual.racecontrol.model.Control;
 
 public class Main {
     public static void main(String[] args) {
-
+        Control control = new Control();
+        control.main(new String[]{});
     }
 }
